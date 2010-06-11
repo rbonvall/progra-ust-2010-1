594edf9aeae1ef67696d14b85c3434dfadf9a1d3
Roberto Bonvallet <rbonvall@gmail.com>
2010-06-11 02:26:19 -0400

    Parametro LdC para split-c3.py

--- evaluaciones/split-c3.py
+++ evaluaciones/split-c3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-programs = open('programas-c3.py').read().split('\n\n')
+
+import sys
+
+programs = open(sys.argv[1]).read().split('\n\n')
 for i, p in enumerate(programs):
     if p.strip():
         open('c3-p{0}.py'.format(i + 1), 'w').write(p)

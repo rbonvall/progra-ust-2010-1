0d41ec91e90e0451345fd447c7268484aaad9ded
Roberto Bonvallet <rbonvall@gmail.com>
2010-07-20 15:57:38 -0400

    Script split ya no es esp.del c3

--- evaluaciones/split.py
+++ evaluaciones/split.py
@@ -5,6 +5,6 @@ import sys
 programs = open(sys.argv[1]).read().split('\n\n')
 for i, p in enumerate(programs):
     if p.strip():
-        open('c3-p{0}.py'.format(i + 1), 'w').write(p)
+        open('p{0}.py'.format(i + 1), 'w').write(p)
 
 
